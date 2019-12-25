06d44cb2280785cf8c5e1d2007090131eaf4651f
Vishnu Divakar <vishnudivakar31@gmail.com>
2019-12-24 20:49:19 -0500

    babel integration

--- paymentmanagement/app.js
+++ paymentmanagement/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send("Home4U")
+    res.send("Home4U Homepage.")
 })
 
 module.exports = app;
